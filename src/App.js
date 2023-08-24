4e246256cb99d6d4b1fc2a72efcd456ce8e69a0a
Karan Khatik <karan13699@gmail.com>
2023-08-24 22:23:15 +0530

    Project Setup - Done

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+
+
+
+
+
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      React App set up Done
+    </div>
+  );
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout />);
